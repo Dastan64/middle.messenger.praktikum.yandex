cd3cf2ae7bc5804bd7984499991fbcb53d82cba2
Dastan Zhamekeshev <dastan132000@mail.ru>
2023-09-26 12:20:12 +0600

    feat: сверстал модуль изменения данных профиля

--- src/pages/profile/modules/change-profile-data/change-profile-data.tmpl.js
+++ src/pages/profile/modules/change-profile-data/change-profile-data.tmpl.js
@@ -1,3 +1,41 @@
 export const tmpl = `
-    
+    <section class="data">
+        <div class="data__avatar-container">
+            <img class="avatar" width="130" height="130" src="https://w.forfun.com/fetch/11/11a048e1dc27122485346049cd6d7c4f.jpeg" alt="">
+        </div>
+        <h3>Дастан Жамекешев</h3>
+        <form class="data__form">
+            <div class="data__input-container">
+                <label for="login" class="data__label">Логин:</label>
+                <input class="data__input input" type="text" name="login" id="login">
+            </div>
+            
+            <div class="data__input-container">
+                <label for="email" class="data__label">E-mail:</label>
+                <input class="data__input input" type="email" name="email" id="email">
+            </div>
+                        
+            <div class="data__input-container">
+                <label for="first_name" class="data__label">Имя:</label>
+                <input class="data__input input" type="text" name="first_name" id="first_name">
+            </div>
+                        
+            <div class="data__input-container">
+                <label for="second_name" class="data__label">Фамилия:</label>
+                <input class="data__input input" type="text" name="second_name" id="second_name">
+            </div>
+                        
+            <div class="data__input-container">
+                <label for="phone" class="data__label">Номер телефона:</label>
+                <input class="data__input input" type="tel" name="phone" id="phone">
+            </div>
+                        
+            <div class="data__input-container">
+                <label for="display_name" class="data__label">Имя в чате:</label>
+                <input class="data__input input" type="text" name="display_name" id="display_name">
+            </div>
+            
+            {{{saveBtn}}}
+        </form>
+    </section>
 `
--- src/pages/profile/modules/change-profile-data/index.js
+++ src/pages/profile/modules/change-profile-data/index.js
@@ -1,7 +1,10 @@
 import Handlebars from 'handlebars';
 import { tmpl } from './change-profile-data.tmpl.js';
 import './change-profile-data.scss';
+import { Button } from '../../../../components/button/index.js';
 
 export const ChangeProfileData = () => {
-    return Handlebars.compile(tmpl)({});
+    return Handlebars.compile(tmpl)({
+        saveBtn: Button({ type: 'submit', className: 'data__save-btn', text: 'Сохранить' }),
+    });
 };
