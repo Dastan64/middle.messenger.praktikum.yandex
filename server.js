31581cf3627b72f320c4780da0f79a074e345ea6
Dastan Zhamekeshev <dastan132000@mail.ru>
2023-09-26 13:41:30 +0600

    fix: переписал server.js на ESM

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
+import path from 'path';
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/dist'));
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, function () {
     console.log(`Приложение открылось на порту: ${PORT}!`);
