1d3b6c85c163327d97d28ed3c693c1dc014d1fa9
Dastan <edaz@technodom.kz>
2023-09-28 19:47:02 +0600

    fix: заменил ссылку аватара из Интернета  на локальную картинку

--- src/components/avatar/avatar.tmpl.js
+++ src/components/avatar/avatar.tmpl.js
@@ -1,7 +1,7 @@
 export const tmpl = `
     <div class="user">
         <div class="user__avatar-container">
-            <img width="130" height="130" src="https://w.forfun.com/fetch/11/11a048e1dc27122485346049cd6d7c4f.jpeg" alt="Фото профиля" class="user__avatar">
+            <img width="130" height="130" src="../../../static/images/avatar.jpeg" alt="Фото профиля" class="user__avatar">
         </div>
         <h1 class="user__name">Дастан Жамекешев</h1>
     </div>
