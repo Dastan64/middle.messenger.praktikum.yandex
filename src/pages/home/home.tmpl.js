e49102529f31dd3b01e071523e7fd984ef925e3f
Dastan <edaz@technodom.kz>
2023-09-29 14:14:47 +0600

    fix: исправил на семантически верную разметку ссылки на главной странице

--- src/pages/home/home.tmpl.js
+++ src/pages/home/home.tmpl.js
@@ -1,9 +1,11 @@
 export const tmpl = `
     <main class="main">
         <h1 class="main__title">Привет, добро пожаловать в чат!</h1>
-        <div class="main__links">
-            {{{profileLink}}}
-            {{{chatsLink}}}
-        </div>
+        <nav class="main__nav">
+            <ul class="main__list">
+                <li class="main__list-item">{{{profileLink}}}</li>
+                <li class="main__list-item">{{{chatsLink}}}</li>
+            </ul>
+        </nav>
     </main>
 `;
