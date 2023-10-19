222fc82561d0a1fd3d256480fd6c014a4f135e19
Жамекешев Дастан <edaz@technodom.kz>
2023-10-19 15:03:49 +0600

    refactor: выделил компонент чатика

--- src/modules/chats/index.ts
+++ src/modules/chats/index.ts
@@ -4,6 +4,8 @@ import { Link } from '../../components/link/index.ts';
 import { Message } from './components/message/index.ts';
 import { ChatMessageForm } from './modules/chat-message-form/index.ts';
 import { Input } from '../../components/input/index.ts';
+import { ChatThumb } from './components/chat-thumb/index.ts';
+import avatar from '../../assets/images/avatar.jpeg';
 
 export class Chats extends Block {
   constructor() {
@@ -24,6 +26,25 @@ export class Chats extends Block {
       new Message({
         text: 'Делись!',
         time: '23:46',
+      }),
+    ];
+
+    this.children.chats = [
+      new ChatThumb({
+        url: '#',
+        name: 'Джонни Депп',
+        avatar,
+        message: 'Привет, Дастан! Просто хотел предупредить',
+        messagesNumber: 13,
+        time: '13:45',
+      }),
+      new ChatThumb({
+        url: '#',
+        name: 'Эмбер Хёрд',
+        avatar,
+        message: 'Привет, Дастан!',
+        messagesNumber: 13,
+        time: '13:46',
       }),
     ];
 
