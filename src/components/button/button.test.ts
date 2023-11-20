8b10c68197bea63cb429e813d302b4596fccdaf8
Дастан Жамекешев <dastan132000@mail.ru>
2023-11-20 16:46:47 +0600

    добавление нового теста и обновление старого

--- src/components/button/button.test.ts
+++ src/components/button/button.test.ts
@@ -20,23 +20,14 @@ describe('Button component', () => {
     expect(callback.calledOnce).to.eq(true);
   });
 
-  it('should have the text passed as an argument', () => {
-    const button = new Button({
-      text: 'button',
-      type: 'button',
-    });
-
-    const element = button.element as HTMLButtonElement;
-    expect(element.textContent).to.eq('button');
-  });
-
-  it('should have the type passed as an argument', () => {
+  it('should have text and type passed as an argument', () => {
     const button = new Button({
       text: 'button',
       type: 'submit',
     });
 
     const element = button.element as HTMLButtonElement;
+    expect(element.textContent).to.eq('button');
     expect(element.type).to.eq('submit');
   });
 });
---
+++ src/components/input/input.test.ts
@@ -0,0 +1,38 @@
+import { describe, it } from 'mocha';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { Input } from './index.ts';
+import '../../utils/registerHelpers.ts';
+
+describe.only('Input component', () => {
+  it('should fire callback on blur', () => {
+    const callback = sinon.stub();
+    const input = new Input({
+      type: 'text',
+      id: '123',
+      name: '123',
+      events: {
+        blur: callback,
+      },
+    });
+
+    const element = input.element as HTMLInputElement;
+
+    element.blur();
+
+    expect(callback.calledOnce).to.equal(true);
+  });
+
+  it('should have the type, id and name passed as an argument', () => {
+    const input = new Input({
+      type: 'text',
+      id: '123',
+      name: '123',
+    });
+
+    const element = input.element as HTMLInputElement;
+    expect(element.type).to.eq('text');
+    expect(element.id).to.eq('123');
+    expect(element.name).to.eq('123');
+  });
+});
